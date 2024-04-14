45b46cd917a9e690f7ceb4c98135c7421c53e5d2
ChillinDylan97 <dylangilbert601@gmail.com>
2024-04-13 23:09:14 -0500

    Stylization complete

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,7 +2,7 @@ import React from  'react';
 import { StyledCell } from './styles/StyledCell';
 import { TETRIMINOES } from '../tetrominoes';
 const Cell = ({ type }) => (
-    <StyledCell type={'L'} color={TETRIMINOES['L'].color}>cell</StyledCell>
+    <StyledCell type={type} color={TETRIMINOES[type].color}/>
 )
 
 export default Cell;
